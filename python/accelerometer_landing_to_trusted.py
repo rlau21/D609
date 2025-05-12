7f1a77c8595b527a74dad0fae761a43fefc7761f
rlau21 <rlau21@wgu.edu>
2025-05-12 14:56:38 -0400

    new python and pics

---
+++ python/accelerometer_landing_to_trusted.py
@@ -0,0 +1,47 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Load tables from the catalog
+accelerometer_landing = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="accelerometer_landing"
+)
+
+customer_trusted = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="customer_trusted"
+)
+
+# Join on user = email
+joined_data = Join.apply(
+    frame1=accelerometer_landing,
+    frame2=customer_trusted,
+    keys1=["user"],
+    keys2=["email"]
+)
+
+# Keep only accelerometer columns
+columns_to_keep = ["user", "timeStamp", "x", "y", "z"]
+accelerometer_trusted = joined_data.select_fields(columns_to_keep)
+
+# Write to Trusted Zone
+glueContext.write_dynamic_frame.from_options(
+    frame=accelerometer_trusted,
+    connection_type="s3",
+    connection_options={"path": "s3://stedi-lakehouse-roblau/accelerometer_trusted/"},
+    format="json"
+)
+
+job.commit()
